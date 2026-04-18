f8c6ffbda00aa03d3c739be8bcd2a936e96f40b5
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-04-18 18:27:23 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -54,7 +54,8 @@ sed -i "14c $masterplus" $usuario/info_panel_control.ini
 indi=$(awk "NR==2" $usuario/MMDVMHost/MMDVM.ini)
 ide=$(awk "NR==3" $usuario/MMDVMHost/MMDVM.ini)
 frec=$(awk "NR==13" $usuario/MMDVMHost/MMDVM.ini)
-masterradio=$(awk "NR==53" "${usuario}/DStarGateway/DStarGateway.ini" | cut -c-30)
+masterradio=$(awk "NR==53" "${usuario}/DStarGateway/DStarGateway.ini" | sed 's/ //g' | cut -c-7)
+
 
 # masterradio=$(awk "NR==53" $usuario/DStarGateway/DStarGateway.ini)
 # masterradio=`expr substr $masterradio 1 30`
