7bbbc2f80ac99593048d880e5588de1bec0c72e2
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-04-20 16:49:33 +0200

    Update extra.php

--- html/extra.php
+++ html/extra.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hola</title>
+    <title>MENU EXTRA</title>
     <style>
         body{
             background: #222;
